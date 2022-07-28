95c63c65935863fbc614cd1c51da09a8bc654d64
Cutler Simpson <cutler.simpson@gmail.com>
2022-07-28 12:41:08 +0900

    Factor in background pixels

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import sys
+from pprint import pprint
 
 stain = cv2.imread('./images/stain.jpg')
 gray_image = cv2.cvtColor(stain, cv2.COLOR_BGR2GRAY)
@@ -8,13 +9,32 @@ histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
 
 minimum = sys.float_info.max
 min_file = ""
+files_dict = {}
 
-for file_name in [f for f in os.listdir('./images/') if f.endswith('.tif')]:
+threshold = 100
+
+for file_name in [f for f in os.listdir('./images') if f.endswith('.tif')]:
     img = os.path.abspath('./images') + "/" + file_name
     image = cv2.imread(img)
+    print("Processing: " + img)
     gray_image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    histogram1 = cv2.calcHist([gray_image1], [0], None, [256], [0, 256])
+    count = 0
+    total = 0
 
+    for i, row in enumerate(gray_image1):
+        for j, pixel in enumerate(gray_image1):
+            pixel = gray_image1[i][j]
+            total += 1
+            if pixel <= threshold:
+                count += 1
+
+
+    percent = (count / float(total))
+    print(percent)
+    if percent > 0.30:
+        continue
+
+    histogram1 = cv2.calcHist([gray_image1], [0], None, [256], [0, 256])
     i = 0
     c1 = 0
     while i<len(histogram) and i<len(histogram1):
@@ -23,10 +43,18 @@ for file_name in [f for f in os.listdir('./images/') if f.endswith('.tif')]:
     c1 = c1**(1 / 2)
     value = c1.item(0)
 
-    if value < minimum:
-        minimum = value
-        min_file = img
+    files_dict.update({value: file_name})
 
-print(minimum)
-print(min_file)
+values = list(files_dict.keys())
+values.sort()
+top_100 = values[:100]
+top_100_dict = {}
+
+for val in top_100:
+    f = files_dict[val]
+    top_100_dict.update({f: val})
+
+print("Top 100 closest images:")
+pprint(list(top_100_dict.keys()))
+
 
