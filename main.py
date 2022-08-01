e536c5b841bd899b077c213122e185e9cb2abadf
Cutler Simpson <cutler.simpson@gmail.com>
2022-08-01 12:21:10 +0900

    Add multiple validation methods

--- main.py
+++ main.py
@@ -2,39 +2,38 @@ import os
 import cv2
 import sys
 from pprint import pprint
+import image_similarity_measures
+from image_similarity_measures.quality_metrics import rmse, ssim, sre
 
-stain = cv2.imread('./images/stain.jpg')
-gray_image = cv2.cvtColor(stain, cv2.COLOR_BGR2GRAY)
-histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
+stain = cv2.imread('./images/stain_snapshot.png')
+gray_stain = cv2.cvtColor(stain, cv2.COLOR_BGR2GRAY)
+histogram = cv2.calcHist([stain], [0], None, [256], [0, 256])
 
-minimum = sys.float_info.max
-min_file = ""
 files_dict = {}
+ssim_measures = {}
+rmse_measures = {}
+sre_measures = {}
+
+scale_percent = 100
+width = int(stain.shape[1] * scale_percent / 100)
+height = int(stain.shape[0] * scale_percent / 100)
+dim = (width, height)
 
 threshold = 100
 
 for file_name in [f for f in os.listdir('./images') if f.endswith('.tif')]:
     img = os.path.abspath('./images') + "/" + file_name
     image = cv2.imread(img)
-    print("Processing: " + img)
+    image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     gray_image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    count = 0
-    total = 0
 
-    for i, row in enumerate(gray_image1):
-        for j, pixel in enumerate(gray_image1):
-            pixel = gray_image1[i][j]
-            total += 1
-            if pixel <= threshold:
-                count += 1
+    print("Processing: " + img)
 
+    rmse_measures[img]= rmse(stain, image)
+    sre_measures[img]= sre(stain, image)
+    ssim_measures[img]= ssim(stain, image)
 
-    percent = (count / float(total))
-    print(percent)
-    if percent > 0.30:
-        continue
-
-    histogram1 = cv2.calcHist([gray_image1], [0], None, [256], [0, 256])
+    histogram1 = cv2.calcHist([image], [0], None, [256], [0, 256])
     i = 0
     c1 = 0
     while i<len(histogram) and i<len(histogram1):
@@ -42,19 +41,31 @@ for file_name in [f for f in os.listdir('./images') if f.endswith('.tif')]:
         i+= 1
     c1 = c1**(1 / 2)
     value = c1.item(0)
+    files_dict.update({img: value})
 
-    files_dict.update({value: file_name})
+def calc_closest_val(dict, checkMax):
+    result = {}
+    if (checkMax):
+    	closest = max(dict.values())
+    else:
+    	closest = min(dict.values())
 
-values = list(files_dict.keys())
-values.sort()
-top_100 = values[:100]
-top_100_dict = {}
+    for key, value in dict.items():
+    	print("The difference between ", key ," and the original image is: ", value)
+    	if (value == closest):
+    	    result[key] = closest
 
-for val in top_100:
-    f = files_dict[val]
-    top_100_dict.update({f: val})
-
-print("Top 100 closest images:")
-pprint(list(top_100_dict.keys()))
+    print("The closest value: ", closest)
+    print("######################################################################")
+    return result
 
 
+ssim = calc_closest_val(ssim_measures, True)
+rmse = calc_closest_val(rmse_measures, False)
+sre = calc_closest_val(sre_measures, True)
+hist = calc_closest_val(files_dict, False)
+
+print("The most similar according to SSIM: " , ssim)
+print("The most similar according to RMSE: " , rmse)
+print("The most similar according to SRE: " , sre)
+print("The most similar according to hist: " , hist)
